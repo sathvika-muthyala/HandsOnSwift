f89fb2b23bfe30b913a6ef820d48bbb2bcdb5cf8
sathvika-muthyala <sathvika141.ms@gmail.com>
2025-10-02 15:46:32 -0400

    images

---
+++ HandsOn/HandsOn/Images.swift
@@ -0,0 +1,18 @@
+//
+//  Images.swift
+//  HandsOn
+//
+//  Created by sathvika muthyala on 10/2/25.
+//
+
+import SwiftUI
+
+struct Images: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Images()
+}
