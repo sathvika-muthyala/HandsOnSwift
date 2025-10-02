83734fcfebfd38195166c41bc2aa7a8363ffc8a1
sathvika-muthyala <sathvika141.ms@gmail.com>
2025-10-02 14:39:47 -0400

    colors added

---
+++ HandsOn/HandsOn/Colorss.swift
@@ -0,0 +1,18 @@
+//
+//  Colorss.swift
+//  HandsOn
+//
+//  Created by sathvika muthyala on 10/2/25.
+//
+
+import SwiftUI
+
+struct Colorss: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Colorss()
+}
