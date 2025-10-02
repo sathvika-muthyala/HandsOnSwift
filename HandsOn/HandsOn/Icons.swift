af2e1a4a83531cc8f956d808c8a91aa0cc8902be
sathvika-muthyala <sathvika141.ms@gmail.com>
2025-10-02 15:28:52 -0400

    icons

---
+++ HandsOn/HandsOn/Icons.swift
@@ -0,0 +1,18 @@
+//
+//  Icons.swift
+//  HandsOn
+//
+//  Created by sathvika muthyala on 10/2/25.
+//
+
+import SwiftUI
+
+struct Icons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Icons()
+}
